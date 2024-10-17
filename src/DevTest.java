8f5d9afbe257b29307eb58775f8ccda2a73a1221
Christian <cristlethedjfurry@gmail.com>
2024-10-17 12:53:22 -0400

    part A complete

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class DevTest
 {
     public static void main(String[] args)
     {
-        
+        Scanner in = new Scanner(System.in);
+        String firstName = "";
+
+        firstName = getNonZeroLenString(in, "Enter your first name: ");
+        System.out.println("Your first name is " + firstName);
+    }
+
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retVal = "";
+
+        do{
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+            if(retVal.isEmpty())
+                System.out.println("You must enter at least one character!");
+
+        }while(retVal.isEmpty());
+        return retVal;
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
+
 public class SafeInput
 {
 
+    /**
+     * gets a string from the user using the console, cant be empty
+     * @param pipe a Scanner used to get the input
+     * @param prompt the prompt that tells the user what to enter
+     * @return a String of at least 1 character in length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retVal = "";
+
+        do{
+            System.out.print(prompt + ": ");
+            retVal = pipe.nextLine();
+            if(retVal.isEmpty())
+                System.out.println("You must enter at least one character!");
+
+        }while(retVal.isEmpty());
+        return retVal;
+    }
 }
